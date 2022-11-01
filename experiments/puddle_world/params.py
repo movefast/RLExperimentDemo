ea9fbae83fd556f95cdc010849b9c7c2f7df8fdf
movefast <liderek269@gmail.com>
2022-11-01 08:40:15 -0600

    run PuddleWorld experiment and evaluate

--- experiments/puddle_world/params.py
+++ experiments/puddle_world/params.py
@@ -33,29 +33,29 @@ params_pw_tc_search_1031 = {
 }
 
 params_pw_tc_1031 = {
-    "Random": {
-        "step_size": [0.25],
+    "QLearning": {
+        "step_size": [0.5],
         'seq_len': [5],
         'opt': ['sgd'],
         'init': [0],
         'tarnetfreq':[1],
     },
-    "QR": {
-        "step_size": [1],
-        'seq_len': [5],
-        'opt': ['sgd'],
-        'init': [0],
-        'tarnetfreq':[1],
-        'num_quant': [2, 4],
-    },
-    "PER": {
-        "step_size": [0.25],
-        'seq_len': [5],
-        'ptype': [1],
-        'per_alpha': [0.4],
-        'importance_sampling': [True],
-        'opt': ['sgd'],
-        'init': [0],
-        'tarnetfreq':[1],
-    },
+    # "QR": {
+    #     "step_size": [1],
+    #     'seq_len': [5],
+    #     'opt': ['sgd'],
+    #     'init': [0],
+    #     'tarnetfreq':[1],
+    #     'num_quant': [2],
+    # },
+    # "PER": {
+    #     "step_size": [0.125],
+    #     'seq_len': [5],
+    #     'ptype': [1],
+    #     'per_alpha': [0.2],
+    #     'importance_sampling': [True],
+    #     'opt': ['sgd'],
+    #     'init': [0],
+    #     'tarnetfreq':[1],
+    # },
 }
